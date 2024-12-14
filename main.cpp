dd0e4490385835ff2e659a70ccdc325e372b38ef
as4g1ri <v.silentovn@gmail.com>
2024-12-15 01:10:17 +0300

    added difference of two numbers

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ int main() {
     int a, b;
     std::cout << "¬ведите два числа: ";
     std::cin >> a >> b;
+    int difference = a - b;
     std::cout << "—умма: " << a + b << std::endl;
+    cout << "The difference of the numbers is: " << difference << endl;
     return 0;
 }
